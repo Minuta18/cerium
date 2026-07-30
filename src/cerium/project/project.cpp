822577e06864d130693c3ff050dd6992e42f2783
Иван Левков <ai.salome@yahoo.com>
2026-07-30 20:37:11 +0300

    add project.cpp and hpp

---
+++ src/cerium/project/project.cpp
@@ -0,0 +1,10 @@
+class Project {
+public:
+    Project() {
+
+    }
+
+    void run() {
+        
+    }
+};
---
+++ src/cerium/project/project.hpp
@@ -0,0 +1,10 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+class Project {
+public:
+    Project();
+    void run();
+};
+
+#endif //PROJECT_H
