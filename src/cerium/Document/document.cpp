9efedee8d8a90d01b74029ccc5ec10cf74b58449
Ivanmaxx <ai.salome@yahoo.com>
2026-08-05 23:25:02 +0300

    fix Document.cpp constructor

--- src/cerium/Document/document.cpp
+++ src/cerium/Document/document.cpp
@@ -1,6 +1,6 @@
 #include "document.hpp"
 
-Document::Document(std::string path, std::string language, bool edit) noexcept {
+Document::Document(Document&&, std::string path, std::string language, bool edit) noexcept {
 	this->path = path;
 	this->language = language;
 	this->edit = edit;
