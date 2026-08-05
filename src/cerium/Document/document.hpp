1640198e4310bd466d5fa58a87fff9ffe1ded60a
Ivanmaxx <ai.salome@yahoo.com>
2026-08-05 20:53:39 +0300

    Add project.hpp and document.hpp parameters and methods

--- src/cerium/Document/document.hpp
+++ src/cerium/Document/document.hpp
@@ -1,7 +1,18 @@
 #ifndef CERIUM_DOCUMENT_DOCUMENT_HPP_
 #define CERIUM_DOCUMENT_DOCUMENT_HPP_
 
+#include <string>
+#include "../project/project.hpp"
+
 class Document {
+	friend class Project;
+private:
+	std::string path;
+	std::string language = "plaintext";
+	bool edit = true;
+	bool open = false;
+	Document();
+public:
 
 };
 
--- src/cerium/project/project.hpp
+++ src/cerium/project/project.hpp
@@ -2,13 +2,26 @@
 #define CERIUM_PROJECT_PROJECT_HPP_
 
 #include <string>
+#include <unordered_map>
+#include <memory>
+
+#include "../Document/document.hpp"
 
 class Project {
 private:
     std::string name;
+    std::unordered_map<std::filesystem::path, std::unique_ptr<Document>> documents;
 
 public:
     Project();
+
+    bool set_name(std::string name);
+
+    void open_document();
+    void close_document();
+	Document get_document(std::string path);
+    void save_document();
+    bool save_document_as(std::string path);
 };
 
 #endif //CERIUM_PROJECT_PROJECT_HPP_
